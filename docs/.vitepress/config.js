4924899553b501f22f33d422018bdb69a5f46152
Akhil  R <akhilr@Akhils-MacBook-Pro.local>
2025-02-14 10:04:42 +0530

    Build website and deploy to GitHub

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "CAPS Online Learning Center",
   description: "A repo of information",
+  base: '/repo/',
   themeConfig: {
     search: {
       provider: 'local'
